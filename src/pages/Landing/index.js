27ebeb997220d27dce98e54fd41903f54e015432
Gavin Callander <gavincallander@Gavins-MacBook-Pro.local>
2020-03-26 11:17:15 -0400

    updating landing need to work out how this will look on mobile if being sent from facebook need to see if Nina can source font

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -8,6 +8,14 @@ export const Landing = props => {
         <div className='landing'>
             <div className='landing landing-banner'>
                 <Header />
+                <div className='landing-banner-message'>
+                    <p className='heading-one'>
+                        Health clinics are in dire need of masks and other supplies
+                    </p>
+                    <p className='body-one landing-banner-text'>
+                        Help fight the spread and protect healthcare workers by sewing or delivering supplies for your community clinics
+                    </p>
+                </div>
             </div>
             <div className='landing landing-donate'></div>
             <Footer />

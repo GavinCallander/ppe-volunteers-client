0d8b9a31f41ef1f45d3cb544b04e1e1aae786eb2
kennansalisbury <kennansalisbury@gmail.com>
2020-03-26 15:00:40 -0700

    basic sign up showing on front end, versioned by clinic and volunteer

--- src/components/modals/Signup.js
+++ src/components/modals/Signup.js
@@ -1,50 +1,32 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import {CallToAction} from '../CallToAction'
+import SignupForm from './SignupForm'
+
 
 const Signup = props => {
-   
-    let [email, setEmail] = useState('')
-    
-    let [password, setPassword] = useState('')
-    let [message, setMessage] = useState('')
-    
-    const handleSubmit = e => {
-        
-        e.preventDefault()
 
-        //post to signup
+    let callToActionButtons = ''
+    if(!props.signupType) {
+        callToActionButtons = (
+            <div>
+                <CallToAction text="VOLUNTEER" setSignupType={props.setSignupType} /> 
+                <CallToAction text="REQUEST SUPPLIES" setSignupType={props.setSignupType} />
+            </div>
+        )
     }
 
-
-    if(!props.showSignup) {
+    if(!props.showSignup && !props.signupType) {
         return null
     }
-    else {
+    else if(props.showSignup) {
+     
         return (
-            //FORM NEEDS VALIDATION MESSAGES ON FRONT END
             <div className='modal'>
                 <p className='close-x' onClick={() => props.closeModal()}>X</p>
                 <div className='modal-content'>
                     <p className='body-one modal-header'>Sign Up</p>
-                    <form className='modal-form' onSubmit={handleSubmit}>
-                        <label>Email</label>
-                        <input type="text" value={email} onChange={e => setEmail(e.currentTarget.value)} />
-                    
-                        <label>Password</label>
-                        <input type="password" value={password} onChange={e => setPassword(e.currentTarget.value)} />
-                        <small>Password field is case sensitive.</small>
-
-                        <label>Display name</label>
-                        <input type="text" value={username} onChange={e => setUsername(e.currentTarget.value)}/>
-                        <small>This will be the name displayed to others with access to the site.</small>
-
-
-                    
-                        <input type="submit" value="Sign Up"/>
-
-                        {/* error messages show here */}
-                        {message}
-
-                    </form>
+                    {callToActionButtons}
+                    <SignupForm signupType={props.signupType} />
                 </div>
                 
             </div>
--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -13,9 +13,13 @@ export const Landing = props => {
     
     let [showSignup, setShowSignup] = useState(false)
     let [showLogin, setShowLogin] = useState(false)
+    let [signupType, setSignupType] = useState('')
     
     const closeModal = () => {
-        if(showSignup) { setShowSignup(false) }  
+        if(showSignup) { 
+            setShowSignup(false) 
+            setSignupType('')
+        }  
         if(showLogin) { setShowLogin(false) } 
     }
     
@@ -23,47 +27,54 @@ export const Landing = props => {
         <div className='landing'>
             <div className='landing landing-banner'>
                 <Header setShowLogin={setShowLogin} setShowSignup={setShowSignup} user={props.user} updateUser={props.updateUser}/>
-            <Signup showSignup={showSignup} closeModal={closeModal} updateUser={props.updateUser} />
-            <Login showLogin={showLogin} closeModal={closeModal} updateUser={props.updateUser}/>
-            <div className='landing-banner-message'>
-                <p className='heading heading-one'>
-                    Health clinics are in dire need of masks and other supplies
-                </p>
-                <p className='body-two landing-banner-text'>
-                    Help fight the spread and protect healthcare workers by sewing or delivering supplies for your community clinics
-                </p>
-            </div>
-            </div>
-            <div className='landing landing-main'>
-                <div className='landing-main landing-main-box'>
-                    <InfoSmall text='Be a producer' />
-                    <InfoSmall text='Help deliver' />
-                </div>
-                <p className='body-two'>We connect our volunteers with their local clinics</p>
-                <CallToAction text='VOLUNTEER' />
-                <p className='heading heading-two'>Are you a small clinic with a large need?</p>
-                <div className='landing-main landing-main-box'>
-                    <InfoLarge text='Masks' />
-                    <InfoLarge text='Face Shields' />
-                    <InfoLarge text='Gowns' />
-                </div>
-                <br />
-                <CallToAction text='REQUEST SUPPLIES' />
-                <br />
-            </div>
-            <div className='landing landing-donate' name='donate'>
-                <div className='landing-donate-message'>
-                    <p className='heading heading-one'>Donate</p>
-                    <p className='body-two landing-donate-text'>
-                        Your contributions cover the cost to volunteers for materials and delivery
+                <Signup 
+                    showSignup={showSignup}
+                    setShowSignup={setShowSignup}
+                    closeModal={closeModal} 
+                    updateUser={props.updateUser} 
+                    signupType={signupType}
+                    setSignupType={setSignupType}
+                />
+                <Login showLogin={showLogin} closeModal={closeModal} updateUser={props.updateUser}/>
+                <div className='landing-banner-message'>
+                    <p className='heading heading-one'>
+                        Health clinics are in dire need of masks and other supplies
                     </p>
-                    <CallToAction text='DONATE MONEY' />
-                    <br />
-                    <CallToAction text='DONATE SUPPLIES' />
+                    <p className='body-two landing-banner-text'>
+                        Help fight the spread and protect healthcare workers by sewing or delivering supplies for your community clinics
+                    </p>
                 </div>
-            </div>
+                </div>
+                <div className='landing landing-main'>
+                    <div className='landing-main landing-main-box'>
+                        <InfoSmall text='Be a producer' />
+                        <InfoSmall text='Help deliver' />
+                    </div>
+                    <p className='body-two'>We connect our volunteers with their local clinics</p>
+                    <CallToAction text='VOLUNTEER' setShowSignup={setShowSignup} signupType={signupType} setSignupType={setSignupType} />
+                    <p className='heading heading-two'>Are you a small clinic with a large need?</p>
+                    <div className='landing-main landing-main-box'>
+                        <InfoLarge text='Masks' />
+                        <InfoLarge text='Face Shields' />
+                        <InfoLarge text='Gowns' />
+                    </div>
+                    <br />
+                    <CallToAction text='REQUEST SUPPLIES' setShowSignup={setShowSignup} signupType={signupType} setSignupType={setSignupType}/>
+                    <br />
+                </div>
+                <div className='landing landing-donate' name='donate'>
+                    <div className='landing-donate-message'>
+                        <p className='heading heading-one'>Donate</p>
+                        <p className='body-two landing-donate-text'>
+                            Your contributions cover the cost to volunteers for materials and delivery
+                        </p>
+                        <CallToAction text='DONATE MONEY' />
+                        <br />
+                        <CallToAction text='DONATE SUPPLIES' />
+                    </div>
+                </div>
 
-            <Footer />
+             <Footer />
         </div>
     )
 };
